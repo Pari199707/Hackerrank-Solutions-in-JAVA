25b7297178d6ef14b29bedbe2d729a7b41d2e803
Pari199707 <121976856+Pari199707@users.noreply.github.com>
2023-07-31 12:13:22 +0530

    Add files via upload

---
+++ timeConversion.java
@@ -0,0 +1,13 @@
+public static String timeConversion(String s) {
+    String start=s.substring(0,2) , end=s.substring(2,8);
+    int hour=Integer.parseInt(start);
+    if (s.contains("P")&& hour<12){
+        hour+=12;
+        start=String.valueOf(hour);
+    }
+    else if (s.contains("A")&& hour==12){
+        start="00";
+    }
+    s=start+end;
+    return s;
+}
